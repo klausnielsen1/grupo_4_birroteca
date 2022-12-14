d2818b9192aa1f81645e4f496485c8e3200f4bff
klausnielsen1 <klaus.nielsen97@gmail.com>
2022-12-14 16:28:28 -0300

    estructura basica login, y configuracion app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+
+let PORT = 3001;
+app.use(express.static("public"));
+app.listen(PORT,()=>console.log("sevidor funcionando en el servidor"+PORT))
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
