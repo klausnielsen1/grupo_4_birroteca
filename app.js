3236ffad1364ba7d1ea0acc21ac7a6644aa6ab62
tomashof01 <tomashofstetter2000@gmail.com>
2022-12-21 17:18:14 -0300

    agregando estructura de css y html detalle producto

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ app.get('/',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/login.html'))
 })
+
+app.get('/product',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/product-detail.html'))
+})
