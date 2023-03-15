298224b35ec79587c138979ad1c04f86d5ae1271
tita <agostina.tizzano@est.fi.uncoma.edu.ar>
2023-03-15 17:41:27 -0300

    clientes.js de nuevo

--- database/models/Clientes.js
+++ database/models/Clientes.js
@@ -1,46 +1,47 @@
 //2 parámetros, el primero es la conexión de la BD y el segundo son los tipos de BD para trabajar 
 module.exports = ( sequelize, dataTypes) =>{
+    let alias = "clientes";
+    let cols={
+       
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre: {
+            type: dataTypes.STRING,
 
-    const clientes = sequelize.define('clientes',
-            id: {
-                type: dataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true
-            },
-            nombre: {
-                type: dataTypes.INTEGER,
+        },
+        apellido:{
+            type: dataTypes.STRING,
 
-            },
-            apellido:{
-                type: dataTypes.INTEGER,
+        },
+        email: {
+            type: dataTypes.STRING,
 
-            },
-            email: {
-                type: dataTypes.INTEGER,
+        },
+        contraseña: {
+            type: dataTypes.STRING,
 
-            },
-            contraseña: {
-                type: dataTypes.INTEGER,
+        },
+        fechaNac:{
+            type: dataTypes.DATE,
 
-            },
-            fechaNac:{
-                type: dataTypes.INTEGER,
+        },
+        avatar: {
+            type: dataTypes.INTEGER,
 
-            },
-            avatar: {
-                type: dataTypes.INTEGER,
+        },
+        id_clase: {
+            type: dataTypes.INTEGER,
+        }
+    }
+    let config = {
+        tableName: "clientes",
+        timestamps:false
+    }
 
-            },
-            id_clase{
-                type: dataTypes.INTEGER,
-
-            }
-
-
-
-    )
-//DEFINE recibe 3 parámetros. 1: nombre de la tabla (nombre del archivo en plural). Es
-//un objeto literal que representa los detalles de la tabla
+    const clientes= sequelize.define(alias,cols,config);
 
 
     return clientes;
